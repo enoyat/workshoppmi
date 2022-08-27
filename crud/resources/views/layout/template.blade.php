0949fec351a3dd5c35b0d5f1dcba89c2b221b362
enoyat <50427196+enoyat@users.noreply.github.com>
2022-08-27 16:01:32 +0700

    2

--- crud/resources/views/layout/template.blade.php
+++ crud/resources/views/layout/template.blade.php
@@ -24,6 +24,7 @@
             <li class="nav-item">
                 <a class="nav-link" href="{{ route('pegawai.index') }}">Pegawai</a>
             </li>
+         </a>
             {{-- <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
                 <div class="dropdown-menu" aria-labelledby="dropdownId">
@@ -32,9 +33,9 @@
                 </div>
             </li> --}}
         </ul>
-        <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="text" placeholder="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        <form class="form-inline my-2 my-lg-0" action="/logout" method="POST">
+            @csrf
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Logout</button>
         </form>
     </div>
 </nav>
--- crud/routes/web.php
+++ crud/routes/web.php
@@ -18,9 +18,12 @@ Route::get('/', function () {
 });
 Route::get('home',[HomeController::class,'index'])->name('home');
 
-Route::get('pegawai',[PegawaiController::class,'index'])->name('pegawai.index');
-Route::get('pegawai/create',[PegawaiController::class,'create'])->name('pegawai.create');
-Route::post('pegawai/store',[PegawaiController::class,'store'])->name('pegawai.store');
-Route::get('pegawai/edit/{id}',[PegawaiController::class,'edit'])->name('pegawai.edit');
-Route::post('pegawai/update/{id}',[PegawaiController::class,'update'])->name('pegawai.update');
-Route::delete('pegawai/destroy/{id}',[PegawaiController::class,'destroy'])->name('pegawai.destroy');
+Auth::routes();
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('pegawai',[PegawaiController::class,'index'])->name('pegawai.index');
+    Route::get('pegawai/create',[PegawaiController::class,'create'])->name('pegawai.create');
+    Route::post('pegawai/store',[PegawaiController::class,'store'])->name('pegawai.store');
+    Route::get('pegawai/edit/{id}',[PegawaiController::class,'edit'])->name('pegawai.edit');
+    Route::post('pegawai/update/{id}',[PegawaiController::class,'update'])->name('pegawai.update');
+    Route::delete('pegawai/destroy/{id}',[PegawaiController::class,'destroy'])->name('pegawai.destroy');
+});
