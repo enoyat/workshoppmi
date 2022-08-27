6fda3d62027ff3189f0589c78cd86e6874823457
enoyat <50427196+enoyat@users.noreply.github.com>
2022-08-27 14:59:13 +0700

    v1

---
+++ crud/app/Pegawai.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Pegawai extends Model
+{
+    protected $table="pegawai";
+    protected $fillable= [
+        'nip',
+        'nama',
+        'alamat'
+    ];
+    //
+}
---
+++ crud/resources/views/dashboard.blade.php
@@ -0,0 +1,12 @@
+@extends('layout.template')
+@section('content')
+<br>
+<div class="jumbotron">
+    <h1 class="display-4">Selamat Datang</h1>
+    <p class="lead">Selamat Datang di SIMPEG UDD</p>
+    <hr class="my-4">
+    <p>UDD (unit Donor Darah)</p>
+</div>
+<br>
+
+@endsection
